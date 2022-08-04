b9dde5d1351c480f0a938a5297cccf5d26f33050
tborist <tborist@yandex.ru>
2022-08-04 09:02:04 +0300

    Trololo

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,10 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+n = input('Введите целое положительное число: ')
+nn = n + n
+nnn = n + n + n
+print("n + nn + nnn= ", int(n) + int(nn) + int(nnn))
+
+
+
