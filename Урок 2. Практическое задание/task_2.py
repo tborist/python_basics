c181c03c559e7576626f892ae27e3c6fefb4df6c
Boris Tadjibaev <tborist@yandex.ru>
2022-08-07 01:22:27 +0300

    task5 in lesson2 !!!

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,14 +11,19 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
-list_1 = input("ведите значения: ").split()
-print(f"Ваш список: ", list_1)
-for i in list_1:
-    a = list_1.index(i)
-    if a % 2 != 0:
-        list_1[a], list_1[a - 1] = list_1[a - 1], list_1[a]
-print(f"Ваш список после обработки: ",list_1)
+#list_1 = input("ведите значения: ").split()
+#rint(f"Ваш список: ", list_1)
+#for i in list_1:
+#    a = list_1.index(i)
+#    print(list_1.index(i))
+#    if a % 2 != 0:
+#        list_1[a], list_1[a - 1] = list_1[a - 1], list_1[a]
+#print(f"Ваш список после обработки: ", list_1)
 
-#my_list = input("Введите целые числа через пробел ").split()
-#my_list[:-1:2], my_list[1::2] = my_list[1::2], my_list[:-1:2]
-#print(my_list)
+my_list = input("Введите целые числа через пробел ").split()
+for i in my_list:
+    print(my_list.index(i))
+my_list[:-1:2], my_list[1::2] = my_list[1::2], my_list[:-1:2]
+print(my_list)
+for i in my_list:
+    print(my_list.index(i))
--- Урок 2. Практическое задание/task_5.py
+++ Урок 2. Практическое задание/task_5.py
@@ -15,3 +15,16 @@
 Набор натуральных чисел можно задать непосредственно в коде,
 например, my_list = [7, 5, 3, 3, 2].
 """
+my_list = [7, 5, 3, 3, 2]
+print(my_list)
+a = int(input("Введите новый элемент рейтинга: "))
+for i in my_list:
+    if a == 1 or a == 2:
+        my_list.append(a)
+        print(my_list)
+        exit()
+    elif a > i:
+        my_list.insert(my_list.index(i), a)
+        print(my_list)
+        exit()
+
